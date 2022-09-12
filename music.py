c5f1f9fe3d5ccbd9e902218ae4ce8bfe9be6a690
Ramlov <mathias.ramlov10@gmail.com>
2022-09-12 14:50:01 +0200

    Music + Threads

--- music.py
+++ music.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env pybricks-micropython
 
 class Musik(ev3):
-    ev3.speaker.set_volume(100)
     Lydeffekt = 0
+
     def musik_play():
         ev3.speaker.play_file("Pornhub-30.rsf")
-    wait(1000)
+    
+
+
 
     
